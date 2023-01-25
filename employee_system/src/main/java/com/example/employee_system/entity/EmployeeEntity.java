422fe04bb30d8f272a2eacf7224dd1c4f228a13d
Pushpendra-Sapkale <61443446+vaishnavisapkale@users.noreply.github.com>
2023-01-25 19:39:58 +0530

    Updated employee management backend
    Added Employee Backend

---
+++ employee_system/src/main/java/com/example/employee_system/entity/EmployeeEntity.java
@@ -0,0 +1,21 @@
+package com.example.employee_system.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "employyes")
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private long id;
+    private String  firstName;
+    private String  lastName;
+    private String email;
+}
